ad42906d94a5e049382541591c10d0b1d3aaf881
bhavani780 <bhavaniravinder16@gmail.com>
2025-02-10 22:34:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -46,37 +46,25 @@ app.get("/contacts/", async (request, response) => {
 app.post("/contacts/", async (request, response) => {
   const contactDetails = request.body;
   const {
+    contactId,
     name,
     email,
-    rating,
-    ratingCount,
-    reviewCount,
-    description,
-    pages,
-    dateOfPublication,
-    editionLanguage,
-    price,
-    onlineStores,
-  } = bookDetails;
-  const addBookQuery = `
+    phoneno,
+    address,
+  } = contactDetails;
+  const addContactQuery = `
     INSERT INTO
-      book (title,author_id,rating,rating_count,review_count,description,pages,date_of_publication,edition_language,price,online_stores)
+      book (name,email,phoneno,address)
     VALUES
       (
-        '${title}',
-         ${authorId},
-         ${rating},
-         ${ratingCount},
-         ${reviewCount},
-        '${description}',
-         ${pages},
-        '${dateOfPublication}',
-        '${editionLanguage}',
-         ${price},
-        '${onlineStores}'
+         ${contactId},
+        '${name}',
+        '${email}',
+         ${phoneNo},
+        '${address}',
       );`;
 
-  const dbResponse = await db.run(addBookQuery);
-  const bookId = dbResponse.lastID;
-  response.send({ bookId: bookId });
+  const dbResponse = await db.run(addContactQuery);
+  const contactId = dbResponse.lastID;
+  response.send({ contactId: contactId });
 });        
